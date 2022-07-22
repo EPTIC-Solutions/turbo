e0fb63840e4c65b6c75f08444a3ce040949bc35f
wizzymore <wizzymore@users.noreply.github.com>
2022-07-22 23:50:26 +0000

    Fix styling

--- tests/TurboFrameTest.php
+++ tests/TurboFrameTest.php
@@ -4,18 +4,19 @@ use Eptic\Turbo\Exceptions\TurboStreamResponseFailedException;
 use Illuminate\Support\Facades\File;
 use Illuminate\View\Factory;
 
-
 beforeEach(function () {
     $viewFactory = app(Factory::class);
     $viewFactory->addLocation('tests/views');
 });
 
 it('throws missing partial exception', function () {
-    $this->assertThrows(function () {
+    $this->assertThrows(
+        function () {
         response()->turboFrame()->toResponse(request());
     },
         TurboStreamResponseFailedException::class,
-        'Missing partial: non "remove" Turbo Streams need a partial.');
+        'Missing partial: non "remove" Turbo Streams need a partial.'
+    );
 });
 
 it('generates correct generic turbo frame', function () {
